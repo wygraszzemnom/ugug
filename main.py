45ebb0cdf28dde27820c107cc6a3650b1ffa2537
rchild09 <rchild09@itstep.local>
2023-10-08 13:29:20 +0200

    dodaem pliki

---
+++ main.py
@@ -0,0 +1,50 @@
+"""
+This is the Template Repl for Python with Turtle.
+
+Python with Turtle lets you make graphics easily in Python.
+
+Check out the official docs here: https://docs.python.org/3/library/turtle.html
+"""
+
+import turtle
+
+# Fullscreen the canvas
+screen = turtle.Screen()
+screen.setup(1.0, 1.0)
+
+# Begin!
+t = turtle.Turtle()
+
+
+# for c in ['red', 'green', 'blue', 'yellow']:
+
+t.forward(170)
+t.left(90)
+turtle.circle(60, -180)
+t.forward(120)
+t.left(90)
+t.forward(170)
+t.left(180)
+t.forward(170)
+t.right(90)
+t.forward(50)
+t.forward(50)
+t.left(120)
+t.forward(105)
+t.left(140)
+t.forward(90)
+t.right(80)
+t.forward(70)
+t.right(180)
+t.forward(190)
+t.right(120)
+t.forward(70)
+t.right(120)
+t.forward(70)
+t.left(60)
+t.forward(120)
+t.left(120)
+t.forward(70)
+t.left(120)
+t.forward(70)
+
