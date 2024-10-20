ef1487d58acf5e54f1857229b95cb5da88cc7380
wygraszzemnom <147312802+wygraszzemnom@users.noreply.github.com>
2024-10-20 13:07:08 +0200

    Update and rename zmienna to zmienna.py

---
+++ zmienna.py
@@ -0,0 +1,34 @@
+def funkcja(x, y):
+    wynik = x + y - 2 * x + 5
+    return wynik
+
+
+wiek = int(input("podaj swój wiek: "))
+wzrost = int(input("podaj swój wzrost: "))
+
+ID = funkcja(wiek, wzrost)
+print(f"twoje id to {ID}")
+
+
+def funkcja(x, y, z, u):
+    wynik = x + y + z + u
+    print(wynik)
+
+liczba1 = int(input("podaj pierwszą liczbe: "))
+liczba2 = int(input("podaj drugą liczbe: "))
+liczba3 = int(input("podaj trzecią liczbe: "))
+liczba4 = int(input("podaj czwartą liczbe: "))
+dodawanie(liczba1, liczba2, liczba3, liczba4)
+
+def funkcja():
+    wynik = 15
+    print(wynik)
+
+def funkcja():
+    imie = "yelysei"
+    nazwisko = "serbin"
+    print(imie, nazwisko)
+    return imie
+
+tylko_imie = funkcja()
+print(tylko_imie)
